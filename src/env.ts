856fb5ac58e9fcf1414ecfdb0932b30a6c8b15c8
Marcin <zolkiewski.marcin@gmail.com>
2021-07-13 16:56:09 +0200

    cleanup

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,3 @@
-export const CHAIN_ID = Number(process.env.REACT_APP_CHAIN_ID || "1");
 export const API_URL = process.env.REACT_APP_API_URL as string;
-export const RPC_URL = process.env.REACT_APP_RPC_URL as string;
-
 export const VALIDATE_UI_URL = process.env.REACT_APP_VALIDATE_UI_URL as string;
 export const APP_UI_URL = process.env.REACT_APP_APP_UI_URL as string;
